e44ca33e88fb1e62665ec33ca2a6a48240469f9b
Joozie <joyce.pebesma@gmail.com>
2014-05-06 14:41:57 +0200

    second

---
+++ session1/result1.sql
@@ -0,0 +1,49 @@
+INSERT INTO Problem1
+(task, result, motivation)
+VALUES
+(a, FALSE, "2 ,3, verschillende auto's kunnen dezelfde capaciteit bevatten, als je de capaciteit weet ken je de auto nog niet.");
+
+INSERT INTO Problem1
+(task, result, motivation)
+VALUES
+(b, TRUE, "2, 3, een auto heeft maar één capaciteit.");
+
+INSERT INTO Problem1
+(task, result, motivation)
+VALUES
+(c, TRUE, "8 vermeldt dat het pakje op hooguit één factuur wordt vermeld, deze dus." );
+
+INSERT INTO Problem1
+(task, result, motivation)
+VALUES
+(d, TRUE, "het pakje hoort bij een afleveradres en een factuur, deze zijn altijd allebei aan een pakje verbonden.");
+
+INSERT INTO Problem1
+(task, result, motivation)
+VALUES
+(e, TRUE, "het pakje vermeldt een afleveradres en is daarmee verbonden aan een factuur." );
+
+INSERT INTO Problem1
+(task, result, motivation)
+VALUES
+(f, TRUE, "2, er wordt opgeslagen welke auto's van welke bezorger zijn.");
+
+INSERT INTO Problem1
+(task, result, motivation)
+VALUES
+(g, FALSE, "2, bezorgers kunnen dezelfde auto hebben of er een delen.");
+
+INSERT INTO Problem1
+(task, result, motivation)
+VALUES
+(h, TRUE, "2, een bezorger kan in verschillende auto's rijden.");
+
+INSERT INTO Problem1
+(task, result, motivation)
+VALUES
+(i, FALSE, "een voertuig heeft maar één capaciteit.");
+
+INSERT INTO Problem1
+(task, result, motivation)
+VALUES
+(j, FALSE, "een voertuig heeft misschien meerdere bezorgers die er gebruik van maken, maar maar één capaciteit.");
